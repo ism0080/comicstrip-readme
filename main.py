66a11d50965fd3e0f23d30da96dc52cf3dc3c4eb
Isaac Mackle <isaac.mackle@dominos.com.au>
2020-09-11 11:19:55 +1200

    :construction: add logic to download comic strip

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import base64
 import sys
 import requests
 from github import Github, GithubException
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 START_COMMENT = "<!--START_SECTION:comicstrip-->"
 END_COMMENT = "<!--END_SECTION:comicstrip-->"
@@ -20,12 +22,32 @@ dilbert = os.getenv("INPUT_SHOW_DILBERT")
 xkcd = os.getenv("INPUT_SHOW_XKCD")
 
 
-def get_xkcd():
-    pass
+def get_xkcd() -> str:
+    """Get image url for comic"""
+    if xkcd == 'true':
+        r = requests.get("https://xkcd.com/info.0.json").json()
+
+        return r['img']
+
+    return ""
 
 
-def get_dilbert():
-    pass
+def get_dilbert() -> str:
+    """Get image url for comic"""
+    if dilbert == 'true':
+        date = get_date()
+        r = requests.get(f"https://dilbert.com/strip/{date}")
+        s = BeautifulSoup(r.text, "html.parser")
+        tag = s.find("meta", attrs={"property": "og:image"})
+
+        return tag.attrs['content']
+
+    return ""
+
+
+def get_date():
+    """Get today's date in format YYYY/MM/DD"""
+    return datetime.today().strftime('%Y-%m-%d')
 
 
 def build_comicstrip():
