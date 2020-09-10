fbffc1345c49eedb30e03217d893e6471715636f
Isaac Mackle <isaac.mackle@dominos.com.au>
2020-09-11 11:27:51 +1200

    :construction: get readme from github
    download readme from github, perform transformation and reupload to github

--- main.py
+++ main.py
@@ -50,17 +50,33 @@ def get_date():
     return datetime.today().strftime('%Y-%m-%d')
 
 
-def build_comicstrip():
+def build_comicstrip() -> str:
     pass
 
 
-def decode_readme():
-    pass
+def decode_readme(data: str) -> str:
+    """Decode the contents of old readme"""
+    decoded_bytes = base64.b64decode(data)
+    return str(decoded_bytes, 'utf-8')
 
 
-def build_readme():
-    pass
+def build_readme(comic: str, readme: str) -> str:
+    """Generate a new README.md"""
+    img = f"{START_COMMENT}\n{comic}\n{END_COMMENT}"
+    return re.sub(listReg, img, readme)
 
 
 if __name__ == '__main__':
-    pass
+    token = Github(ghtoken)
+    try:
+        repo = token.get_repo(repository)
+    except GithubException:
+        print("Authentication Error")
+        sys.exit(1)
+    file = repo.get_readme()
+    comic = build_comicstrip()
+    decoded_file = decode_readme(file.content)
+    updated_readme = build_readme(comic=comic, readme=decoded_file)
+    if updated_readme != decoded_file:
+        repo.update_file(path=file.path, message=commit_message, content=updated_readme, sha=file.sha, branch='master')
+
