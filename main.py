5703d56eb495cf2e6fdcf619bdc1038b8d667a68
Isaac Mackle <isaac.mackle@dominos.com.au>
2020-10-07 16:53:37 +1300

    :construction: format image strings for readme

--- main.py
+++ main.py
@@ -18,16 +18,17 @@ listReg = f"{START_COMMENT}[\\s\\S]+{END_COMMENT}"
 ghtoken = os.getenv("INPUT_GH_TOKEN")
 repository = os.getenv("INPUT_REPOSITORY")
 commit_message = os.getenv("INPUT_COMMIT_MESSAGE")
-dilbert = os.getenv("INPUT_SHOW_DILBERT")
 xkcd = os.getenv("INPUT_SHOW_XKCD")
+dilbert = os.getenv("INPUT_SHOW_DILBERT")
 
 
 def get_xkcd() -> str:
     """Get image url for comic"""
     if xkcd == 'true':
         r = requests.get("https://xkcd.com/info.0.json").json()
+        img = r['img']
 
-        return r['img']
+        return f'<a href="https://xkcd.com/">\n <img src="{img}" />\n</a>'
 
     return ""
 
@@ -39,19 +40,32 @@ def get_dilbert() -> str:
         r = requests.get(f"https://dilbert.com/strip/{date}")
         s = BeautifulSoup(r.text, "html.parser")
         tag = s.find("meta", attrs={"property": "og:image"})
+        img = tag.attrs['content']
 
-        return tag.attrs['content']
+        return f'<a href="https://dilbert.com/strip/{date}">\n <img src="{img}" />\n</a>'
 
     return ""
 
 
-def get_date():
+def get_date() -> str:
     """Get today's date in format YYYY/MM/DD"""
     return datetime.today().strftime('%Y-%m-%d')
 
 
 def build_comicstrip() -> str:
-    pass
+    comicstrip = ""
+    dilbert_comic = get_dilbert()
+    xkcd_comic = get_xkcd()
+    if dilbert_comic and xkcd_comic:
+        comicstrip = f'<p align="center">\n {dilbert_comic}\n {xkcd_comic}\n</p>'
+    if dilbert_comic and not xkcd_comic:
+        comicstrip = f'<p align="center">\n {dilbert_comic}\n</p>'
+    if xkcd_comic and not dilbert_comic:
+        comicstrip = f'<p align="center">\n {xkcd_comic}\n</p>'
+    if not (dilbert_comic or xkcd_comic):
+        return comicstrip
+
+    return comicstrip
 
 
 def decode_readme(data: str) -> str:
@@ -78,5 +92,5 @@ if __name__ == '__main__':
     decoded_file = decode_readme(file.content)
     updated_readme = build_readme(comic=comic, readme=decoded_file)
     if updated_readme != decoded_file:
-        repo.update_file(path=file.path, message=commit_message, content=updated_readme, sha=file.sha, branch='master')
-
+        repo.update_file(path=file.path, message=commit_message,
+                         content=updated_readme, sha=file.sha, branch=repo.default_branch)
