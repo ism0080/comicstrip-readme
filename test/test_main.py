0545e5789af234562e674ec55f743674c1414550
Isaac Mackle <isaac.mackle@dominos.com.au>
2020-10-07 17:49:09 +1300

    :white_check_mark: update tests

--- test/test_main.py
+++ test/test_main.py
@@ -4,17 +4,10 @@ Tests for main.py
 
 import unittest
 import os
-from main import get_dilbert, get_xkcd
+from main import get_xkcd
 
 
 class TestMain(unittest.TestCase):
-
-    def test_dilbert(self):
-        """Tests for downloading dilbert comic"""
-        def test(env: str, result: str):
-            os.environ["INPUT_SHOW_DILBERT"] = env
-            self.assertEqual(get_dilbert(), result)
-        test('false', "")
 
     def test_xkcd(self):
         """Tests for downloading xkcd comic"""
@@ -25,11 +18,10 @@ class TestMain(unittest.TestCase):
 
     def test_comicstrip(self):
         """Tests for the string added to the readme"""
-        def test(dilbert: str, xkcd: str, result: str):
-            os.environ["INPUT_SHOW_DILBERT"] = dilbert
+        def test(xkcd: str, result: str):
             os.environ["INPUT_SHOW_XKCD"] = xkcd
             self.assertEqual(get_xkcd(), result)
-        test('false', 'false', "")
+        test('false', "")
 
 
 if __name__ == '__main__':
